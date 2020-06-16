e105f1e2f346bbf04c0d887ec05212bad0e3d5f2
Meightee <anastasiamelnikv@gmail.com>
2020-06-16 20:04:36 +0300

    add Repositories

---
+++ Infrastructure/BoardRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class BoardRepository : AuditableRepository<Board>, IBoardRepository
+    {
+        public BoardRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+        public Board Get(int id)
+        {
+            return DbContext.Find<Board>(id);
+        }
+        public void Remove(Board entity)
+        {
+            DbContext.Remove(entity);
+        }
+    }
+}
---
+++ Infrastructure/IBoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using Infrastructure.CRUDInterfaces;
+
+namespace Infrastructure
+{
+    public interface IBoardRepository : ICanAdd<Board>, ICanDelete<Board>, ICanGet<Board>, ICanUpdate<Board>
+    {
+    }
+}
