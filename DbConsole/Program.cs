9b042f4595f81b5b6ff7102895d774eaababef36
Meightee <anastasiamelnikv@gmail.com>
2020-06-16 19:21:14 +0300

    add DbConsole

---
+++ DbConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace DbConsole
+{
+    //Фабрика для создания объекта контекста базы данных во время разработки
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Forrrum;Trusted_Connection=True;", b => b.MigrationsAssembly("Infrastructure"));
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
